9e9cc5345dadc605aefdc33e3a2e1ac35026a40b
Ryuukaiser <86337534+Ryuukaiser@users.noreply.github.com>
2021-07-13 23:51:36 +0800

    Revert "B5 phần 2"
    This reverts commit b10bf41b891c6320f9f303950c10cd6644c2551b.

--- btlab4/Controllers/CoursesController.cs
+++ btlab4/Controllers/CoursesController.cs
@@ -40,28 +40,6 @@ namespace btlab4.Controllers
             return RedirectToAction("index", "Home");
 
         }
-        public ActionResult Attending()
-        {
-             ApplicationUser currentUser  = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
-             var listAttendances = context.Attendances.Where(p => p.Attendee == currentUser.Id).ToList();
-            var courses = new List<Course>();
-            foreach (Attendance temp in listAttendances)
-            {
-                Course objCourse = temp.Course;
-                objCourse.LectureName = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(objCourse.LecturerId).Name;
-                courses.Add(objCourse);
-            }   
-            return View(courses);
-        }
-        public ActionResult Mine()
-        {
-            ApplicationUser currentUser = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
-            var courses = context.Courses.Where(c => c.LecturerId == currentUser.Id && c.DateTime > DateTime.Now).ToList(); 
-            foreach (Course i in courses)
-            {
-                i.LectureName = currentUser.Name;
-            }
-            return View(courses);
-        }
+
     }
 }
--- btlab4/Models/Course.cs
+++ btlab4/Models/Course.cs
@@ -9,12 +9,6 @@
     [Table("Course")]
     public partial class Course
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Course()
-        {
-            Attendances = new HashSet<Attendance>();
-        }
-
         public int Id { get; set; }
 
         [Required]
@@ -29,16 +23,8 @@
 
         public DateTime DateTime { get; set; }
         [Required(ErrorMessage = "Mã thể loại không được trống ")]
-
         public int CatergoryId { get; set; }
         public string Name = "";
-        public string LectureName = "";
         public List<Category> ListCategory = new List<Category>();
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Attendance> Attendances { get; set; }
-
-        public virtual Category Category { get; set; }
     }
-   
 }
